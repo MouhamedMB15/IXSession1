ac58e4f189e0490bb263d8bfd44c7bdab4c1b36c
Mouhamed Mbengue <mouhamed23@Mouhameds-MacBook-Pro.local>
2024-05-21 14:26:38 +0200

    Day 2 HW developed in the index.js file for fibonacci and triangle functions using js

--- index.js
+++ index.js
@@ -1,10 +1,73 @@
 
 
-const burger = document.querySelector('.navbar__burger');
-const navLinks = document.querySelector('.navbar__links');
+//HOME WORK ASSIGNMENT DAY 2 SWE IX
+//FIBINCOCCI SEQUENCE IN JS FUNCTIONS
+//TUE MAY 21, 2024
 
-burger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    //animate burger lines into an X
-    burger.classList.toggle('toggle');
-});
+//Fibinoicc Sequence
+function nthFib(n) {
+    if(n <= 1) {
+        return n;
+    }
+    else {
+        //recursive return sum of fibs
+        return nthFib(n-1) + nthFib(n-2);
+    }
+}
+
+//Digits up to n Fib
+function fib(n) {
+    //base case
+    if(n === 0) {
+        return [];
+    }
+    if(n === 1) {
+        return [n];
+    }
+    if(n === 2) {
+        return [0,1];
+    }
+    //result
+    const result = [0,1];
+    for(let i = 2; i < n; i++) {
+        //push nth fib to arr
+        result.push(result[i-1] + result[i-2]);
+    }
+    return result;
+}
+
+//test Cases for sum of fibs
+console.log("Nth Fibinoicci num.");
+const a = 6; //return 6th fib num
+console.log(`The nth Fibonacci of n = ${a} is ${nthFib(a)}.`);
+const b = 10; //return 10th fib num
+console.log(`The nth Fibonacci of n = ${b} is ${nthFib(b)}.`);
+
+//test cases for arr
+console.log("Nth Fibinoicci num.");
+const c = 10; //n 10
+console.log(`The Fibinocci numbs up to ${c} is ${fib(c)}.`);
+
+
+//Home Work 2 
+function triangleT(tri) {
+    //base case not 3 sides
+    if(tri.length !== 3) {
+        console.log("Not a Triangle");
+    }
+
+    //declar tri
+    const [a,b,c] = tri;
+    //semi prim
+    const s = (a+b+c) / 2;
+    //calc area
+    const area = Math.sqrt(s *(s-a)*(s-b) * (s-c)); //heron form
+
+    return area;
+}
+
+//Test Cases for triangle
+const tri = [7,12,10];
+console.log(`Triangle area for sides ${tri} is ${triangleT(tri)} `);
+
+
